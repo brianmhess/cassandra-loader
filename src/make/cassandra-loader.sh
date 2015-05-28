652824aa2dc7d6484314336d20d0b05e1f7add37
Brian Hess <brianmhess@gmail.com>
2015-05-28 09:46:49 -0700

    JVM options

--- src/make/cassandra-loader.sh
+++ src/make/cassandra-loader.sh
@@ -5,5 +5,5 @@ java=java
 if test -n "$JAVA_HOME"; then
     java="$JAVA_HOME/bin/java"
 fi
-exec "$java" -XX:+UseG1GC -Xmx6G -XX:+UseTLAB -XX:+ResizeTLAB -XX:-UseBiasedLocking -XX:+AlwaysPreTouch $java_args -jar $MYSELF "$@"
+exec "$java" -XX:+UseG1GC -Xmx8G -Xms8G -XX:+UseTLAB -XX:+ResizeTLAB -XX:+AlwaysPreTouch $java_args -jar $MYSELF "$@"
 exit 1 
